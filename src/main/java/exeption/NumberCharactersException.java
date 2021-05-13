8826d0ba49eff9b377e0858017ddec1a67992c82
Vladislav <71547144+BraveSpiritHacker@users.noreply.github.com>
2021-05-13 12:02:45 +0400

    bugfix

--- src/main/java/exeption/NumberCharactersException.java
+++ src/main/java/exeption/NumberCharactersException.java
@@ -1,5 +1,5 @@
 package exeption;
 
 public class NumberCharactersException extends  Exception{
-    public NumberCharactersException(String id){super("Недопустимое количество символов в ID(должно быть < 8) - "+ id);}
+    public NumberCharactersException(String id){super("Недопустимое количество символов в ID(должно быть <= 8) - "+ id);}
 }
